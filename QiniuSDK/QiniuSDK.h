a6043fd6808987b310dc74333aa22f2a01417bcc
luo <luo_ty@qq.com>
2026-03-31 12:28:01 +0800

    headers fix

--- QiniuSDK/QiniuSDK.h
+++ QiniuSDK/QiniuSDK.h
@@ -8,19 +8,113 @@
 
 #import <Foundation/Foundation.h>
 
-#import "QNErrorCode.h"
-#import "QNDns.h"
-#import "QNZone.h"
-#import "QNFixedZone.h"
-#import "QNAutoZone.h"
+// Utils
+#import "NSData+QNGZip.h"
+#import "NSData+QNMD5.h"
+#import "NSObject+QNSwizzle.h"
+#import "QNALAssetFile.h"
+#import "QNAsyncRun.h"
+#import "QNCache.h"
+#import "QNCrc32.h"
+#import "QNDefine.h"
+#import "QNEtag.h"
+#import "QNFile.h"
+#import "QNFileDelegate.h"
 #import "QNLogUtil.h"
+#import "QNMutableArray.h"
+#import "QNPHAssetFile.h"
+#import "QNPHAssetResource.h"
+#import "QNSingleFlight.h"
+#import "QNSystem.h"
+#import "QNUrlSafeBase64.h"
+#import "QNUtils.h"
+#import "QNVersion.h"
+#import "QN_GTM_Base64.h"
+
+// Common
+#import "QNAutoZone.h"
+#import "QNConfig.h"
+#import "QNErrorCode.h"
+#import "QNFixedZone.h"
+#import "QNZone.h"
+#import "QNZoneInfo.h"
+
+// Storage
+#import "QNBaseUpload.h"
+#import "QNConcurrentResumeUpload.h"
 #import "QNConfiguration.h"
-#import "QNServerConfigMonitor.h"
-#import "QNRecorderDelegate.h"
-#import "QNFileRecorder.h"
-#import "QNResponseInfo.h"
+#import "QNFormUpload.h"
+#import "QNPartsUpload.h"
+#import "QNPartsUploadPerformer.h"
+#import "QNPartsUploadPerformerV1.h"
+#import "QNPartsUploadPerformerV2.h"
+#import "QNUpProgress.h"
+#import "QNUpToken.h"
+#import "QNUploadBlock.h"
+#import "QNUploadData.h"
+#import "QNUploadInfo.h"
+#import "QNUploadInfoV1.h"
+#import "QNUploadInfoV2.h"
 #import "QNUploadManager.h"
 #import "QNUploadOption.h"
-#import "QNUrlSafeBase64.h"
+#import "QNUploadSource.h"
+#import "QNUploadSourceFile.h"
+#import "QNUploadSourceStream.h"
+// Storage / ServerConfig
+#import "QNServerConfig.h"
+#import "QNServerConfigCache.h"
+#import "QNServerConfigMonitor.h"
+#import "QNServerConfigSynchronizer.h"
+#import "QNServerUserConfig.h"
+
+// Http
+#import "QNResponseInfo.h"
+#import "QNUploadRequestMetrics.h"
+#import "QNUploadRequestState.h"
+#import "QNUserAgent.h"
+// Http / Dns
+#import "QNDns.h"
+#import "QNDnsCacheFile.h"
+#import "QNDnsCacheInfo.h"
+#import "QNDnsPrefetch.h"
+#import "QNInetAddress.h"
+// Http / ConnectCheck
+#import "QNConnectChecker.h"
+// Http / NetworkStatus
+#import "QNNetworkStatusManager.h"
+#import "QNUploadServerNetworkStatus.h"
+// Http / Request
+#import "QNHttpRegionRequest.h"
+#import "QNHttpSingleRequest.h"
+#import "QNIUploadServer.h"
+#import "QNRequestClient.h"
+#import "QNRequestTransaction.h"
+#import "QNUploadRegionInfo.h"
+#import "QNUploadRequestInfo.h"
+// Http / Request / HttpClient
+#import "NSURLRequest+QNRequest.h"
+#import "QNCFHttpClient.h"
+#import "QNCFHttpClientInner.h"
+#import "QNCFHttpThreadPool.h"
+#import "QNUploadSystemClient.h"
+// Http / ServerRegion
+#import "QNUploadDomainRegion.h"
+#import "QNUploadServer.h"
+#import "QNUploadServerFreezeManager.h"
+#import "QNUploadServerFreezeUtil.h"
+
+// Recorder
+#import "QNFileRecorder.h"
+#import "QNRecorderDelegate.h"
+
+// Collect
 #import "QNReportConfig.h"
+#import "QNReportItem.h"
+#import "QNUploadInfoReporter.h"
+
+// BigData
 #import "QNPipeline.h"
+#import "QNSessionManager.h"
+
+// Transaction
+#import "QNTransactionManager.h"
